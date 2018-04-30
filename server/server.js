f38fe6ada666370780539df8327c08c2ae6e5c86
Recep Alparslan <ralparslan@altar-tr.com>
2018-04-30 21:49:49 +0300

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
+const {generateMessage} = require('./utils/message');
+
 let app = express();
 let server = http.createServer(app);
 
@@ -17,40 +19,9 @@ io.on('connection',(socket,)=>{
   console.log('New user connected');
 
 // admin text Welcome to the chat app
-    socket.emit('newMessage',{
-      from:'Admin',
-      text:'Welcome to chat app',
-      createAt: new Date().getTime()
-    });
+    socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
 
-    socket.broadcast.emit('newMessage',{
-      from:'Admin',
-      text:'New user joined',
-      createAt: new Date().getTime()
-    });
-
-
-
-  // socket.emit('newMessage',{
-  //   from:'Recep',
-  //   text:'see you then',
-  //   createdAt : 1543
-  // });
-
-
-// PUSH
-  // socket.emit('newEmail',{
-  //   from:'alparslanrecep@gmail.com',
-  //   text:'Sa naber',
-  //   createdAt : 1543
-  // });
-
-
-  // socket.emit('newNotification',{
-  //   title:'first notification',
-  //   text:'a new file added to lesson5',
-  //   createdAt : 1543
-  // });
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
 //PULL
   socket.on(('connect'),function(){
@@ -61,11 +32,7 @@ io.on('connection',(socket,)=>{
   socket.on('createMessage', function(message){
     console.log('create message : ' , message);
 
-      io.emit('newMessage',{
-      from:message.from,
-      text:message.text,
-      createAt: new Date().getTime()
-    });
+      io.emit('newMessage',generateMessage(message.from,message.text));
   });
 
   socket.on(('disconnect'),function(){
